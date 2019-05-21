f1c68e65df980a4ef56ef96c167fc04889e436ff
Mamoun1 <diabmam@sheridancollege.ca>
2019-05-21 14:42:13 -0400

    Commit added

--- src/studentdemo/StudentDemo.java
+++ src/studentdemo/StudentDemo.java
@@ -2,6 +2,8 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Updating somthing
+ * May, 21, 2019
  */
 package studentdemo;
 import java.util.Scanner;
